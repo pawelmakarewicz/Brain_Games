08e6b4744fc0d6d0ce77e51ec1bc4be5a460c260
Pawel <pawel@Pawel.localdomain>
2022-08-15 20:19:00 +0200

    step_4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readLineSync from "readline-sync"
-const  greating = () => {
-    let userName = readLineSync.question('May I have your name? ');
-    return console.log('Hello, ' + userName + '!');
-}
-export {greating};
+import readLineSync from 'readline-sync';
 
+const greating = () => {
+  const userName = readLineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
+};
+export { greating };
